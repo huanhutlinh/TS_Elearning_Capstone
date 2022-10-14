10774980c1e10422599bf247e088e30351e224bf
huanhutlinh <huanhutlinh128@gmail.com>
2022-10-15 02:06:17 +0700

    Navbar and footer

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React, { Suspense, lazy } from "react";
+import { Switch, Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+export const history = createBrowserHistory();
+
+function App() {
+  return (
+    <div>
+      <Router history={history}>
+        <Suspense fallback={<Loading />}>
+          <Switch></Switch>
+        </Suspense>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
