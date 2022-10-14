91461ee9a605562d85b0d17ec2b097dab6d18408
huanhutlinh <huanhutlinh128@gmail.com>
2022-10-14 22:50:07 +0700

    Add init

---
+++ utilities/config.ts
@@ -0,0 +1,12 @@
+export const DOMAIN = "https://elearningnew.cybersoft.edu.vn";
+export const TOKEN = "accessToken";
+export const GROUPID = "GP01";
+export const TokenCyberSoft = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyOCIsIkhldEhhblN0cmluZyI6IjAxLzAzLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY3NzYyODgwMDAwMCIsIm5iZiI6MTY0Nzk2ODQwMCwiZXhwIjoxNjc3Nzc2NDAwfQ.z923oFwCROoABao5uzu3qATLUqYBTAaJWQMPJAUBNFU";
+export const TYPE_USER = 'TYPE_USER'; 
+export const USER_SIGNIN = "USER_SIGNIN";
+export const CATE_BACKEND = "BackEnd";
+export const CATE_DESIGN = "Design";
+export const CATE_DIDONG = "DiDong";
+export const CATE_FRONTEND = "FrontEnd";
+export const CATE_FULLSTACK = "FullStack";
+export const CATE_TUDUY = "TuDuy";
